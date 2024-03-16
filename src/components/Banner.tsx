fe358b22426e325d20bccc570b4694428004f863
Pasztor Isti <isticsek@gmail.com>
2024-03-16 15:36:11 +0200

    create test banner

---
+++ src/components/Banner.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <div>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus nulla
+        nihil sit numquam doloremque sapiente, delectus aut, nisi inventore
+        ratione accusamus iste possimus eum asperiores in quod ullam,
+        praesentium quos?
+      </p>
+    </div>
+  );
+};
+
+export default Banner;
